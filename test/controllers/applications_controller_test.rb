ae2528dc997b7e6090a2a7f3962e9181a8333aea
John Ivanoff <john.ivanoff@gmail.com>
2014-04-21 13:45:21 -0500

    starting some tests

--- test/controllers/applications_controller_test.rb
+++ test/controllers/applications_controller_test.rb
@@ -1,7 +1,50 @@
 require 'test_helper'
 
 class ApplicationsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @application = applications(:one)
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:applications)
+  end
+
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create application" do
+    assert_difference('Application.count') do
+      post :create, application: { applied_on: @application.applied_on, company: @application.company, found_on: @application.found_on, position: @application.position, refer: @application.refer}
+    end
+
+    assert_redirected_to application_path(assigns(:application))
+  end
+
+  test "should show application" do
+    get :show, id: @application
+    assert_response :success
+  end
+
+#  test "should get edit" do
+#    get :edit, id: @application
+#    assert_response :success
+#  end
+
+#  test "should update application" do
+#    patch :update, id: @application, application: { applied_on: @application.applied_on, company: @application.company, found_on: @application.found_on, position: @application.position, refer: @application.refer }
+#    assert_redirected_to application_path(assigns(:application))
+#  end
+
+#  test "should destroy application" do
+#    assert_difference('application.count', -1) do
+#      delete :destroy, id: @application
+#    end
+
+#    assert_redirected_to applications_path
+#  end
 end
