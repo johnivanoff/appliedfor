d7f4651485036cf2ae0388a17ada709c64863d85
John Ivanoff <john.ivanoff@gmail.com>
2014-04-25 15:44:11 -0500

    created users and a session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'sessions/new'
+
+  resources :users
+
   resources :applications
 
   # The priority is based upon order of creation: first created -> highest priority.
