1c7e350383522fb564b3b7305cfc66de97745415
John Ivanoff <john.ivanoff@gmail.com>
2014-04-25 15:17:58 -0500

    can now edit and delete applications

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -9,17 +9,34 @@ class ApplicationsController < ApplicationController
   end
   
   def new
-    
   end
   
   def create
     @application = Application.new(application_params)
- 
     @application.save
     redirect_to @application
   end
   
+  def edit
+    @application = Application.find(params[:id])
+  end
   
+  def update
+    @application = Application.find(params[:id])
+ 
+    if @application.update(application_params)
+      redirect_to @application
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @application = Application.find(params[:id])
+    @application.destroy
+
+    redirect_to applications_path
+  end
 
   private
     def application_params
